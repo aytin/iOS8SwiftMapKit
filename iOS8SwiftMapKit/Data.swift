558a7861637b4726e1ba483f42fe3e7d604b0597
kenwilcox <wilcoxk@gmail.com>
2015-01-04 21:57:05 -0700

    Added Some Data

---
+++ iOS8SwiftMapKit/Data.swift
@@ -0,0 +1,30 @@
+//
+//  Data.swift
+//  iOS8SwiftMapKit
+//
+//  Created by Kenneth Wilcox on 1/4/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+
+let Data = [
+  "01-09": ["DisplayAddress": "ANDERSON HALL 305 BATES AVE PARMA, ID 83660",
+    "Address": "305 BATES AVE PARMA, ID 83660",
+    "PollingPlaceName": "ANDERSON HALL",
+    "LocationName": "305 BATES AVE",
+    "LocationDesc": "",
+    "CityStateZip": "PARMA, ID 83660",
+    "Latitude": 43.786266,
+    "Longitude": -116.943101
+  ],
+  "02-09": ["DisplayAddress": "NOTUS COMMUNITY CENTER 389 1ST ST NOTUS, ID 83656",
+    "Address": "389 1ST ST NOTUS, ID 83656",
+    "PollingPlaceName": "NOTUS COMMUNITY CENTER",
+    "LocationName": "389 1ST ST",
+    "LocationDesc": "",
+    "CityStateZip": "NOTUS, ID 83656",
+    "Latitude": 43.725804,
+    "Longitude": -116.800402,
+  ]
+]
