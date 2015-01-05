a83a3ac05e4b291fd52616b9cca0ee6b5fc099a3
kenwilcox <wilcoxk@gmail.com>
2015-01-04 21:44:35 -0700

    Show a map of London

--- iOS8SwiftMapKit/ViewController.swift
+++ iOS8SwiftMapKit/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let location = CLLocationCoordinate2D (latitude: 51.50007773, longitude: -0.1246402)
+    let span = MKCoordinateSpanMake(0.05, 0.05)
+    let region = MKCoordinateRegion(center: location, span: span)
+    mapView.setRegion(region, animated: true)
   }
 
   override func didReceiveMemoryWarning() {
