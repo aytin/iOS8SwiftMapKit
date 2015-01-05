8d7baa2529e223b8567131faa63dbcfc3a9044b1
kenwilcox <wilcoxk@gmail.com>
2015-01-04 22:51:05 -0700

    Actually adding map stuff now

--- iOS8SwiftMapKit/ViewController.swift
+++ iOS8SwiftMapKit/ViewController.swift
@@ -17,10 +17,10 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-//    let location = CLLocationCoordinate2D (latitude: 43.524492, longitude: -116.614745)
-//    let span = MKCoordinateSpanMake(0.85, 0.85)
-//    let region = MKCoordinateRegion(center: location, span: span)
-//    mapView.setRegion(region, animated: true)
+    let location = CLLocationCoordinate2D (latitude: 43.524492, longitude: -116.614745)
+    let span = MKCoordinateSpanMake(0.85, 0.85)
+    let region = MKCoordinateRegion(center: location, span: span)
+    mapView.setRegion(region, animated: true)
     
     //    let annotation = MKPointAnnotation()
     //    annotation.setCoordinate(location)
@@ -51,8 +51,22 @@ class ViewController: UIViewController {
         print(" Lon: \(lon)")
       }
       
+      if let index = find(keys, "PollingPlaceName") {
+        precinct = "\(precinct) - \(values[index])"
+      }
       
+      var address = ""
+      if let index = find(keys, "Address") {
+        address = values[index]
+      }
       println()
+      
+      let precinctLocation = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+      let annotation = MKPointAnnotation()
+      annotation.setCoordinate(precinctLocation)
+      annotation.title = precinct
+      annotation.subtitle = address
+      mapView.addAnnotation(annotation)
     }
   }
   
