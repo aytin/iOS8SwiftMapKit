c06b8d0fec2705108ec75a5903c592f431414ba4
kenwilcox <wilcoxk@gmail.com>
2015-01-04 22:44:09 -0700

    Added Lat and Lon - commented out the map stuff for now

--- iOS8SwiftMapKit/ViewController.swift
+++ iOS8SwiftMapKit/ViewController.swift
@@ -17,10 +17,10 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    let location = CLLocationCoordinate2D (latitude: 43.524492, longitude: -116.614745)
-    let span = MKCoordinateSpanMake(0.85, 0.85)
-    let region = MKCoordinateRegion(center: location, span: span)
-    mapView.setRegion(region, animated: true)
+//    let location = CLLocationCoordinate2D (latitude: 43.524492, longitude: -116.614745)
+//    let span = MKCoordinateSpanMake(0.85, 0.85)
+//    let region = MKCoordinateRegion(center: location, span: span)
+//    mapView.setRegion(region, animated: true)
     
     //    let annotation = MKPointAnnotation()
     //    annotation.setCoordinate(location)
@@ -33,13 +33,26 @@ class ViewController: UIViewController {
       let keys = [String](data.keys)
       let values = [String](data.values)
       
-//      for i in 1..<keys.count {
-//        println("\(keys[i]): \(values[i])")
-//      }
-      
+      var precinct = ""
       if let index = find(keys, "Precinct") {
-        println("Precinct: \(values[index])")
+        precinct = values[index]
+        print("Precinct: \(precinct)")
       }
+      
+      var lat:Float = 0.0
+      if let index = find(keys, "Latitude") {
+        lat = (values[index] as NSString).floatValue
+        print(" Lat: \(lat)")
+      }
+      
+      var lon:Float = 0.0
+      if let index = find(keys, "Longitude") {
+        lon = (values[index] as NSString).floatValue
+        print(" Lon: \(lon)")
+      }
+      
+      
+      println()
     }
   }
   
