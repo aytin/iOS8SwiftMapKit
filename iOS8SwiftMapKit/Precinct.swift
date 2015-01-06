cc51a2e15fcdeaca06b717ecaa4cfe690d1bc5d6
kenwilcox <wilcoxk@gmail.com>
2015-01-05 22:47:22 -0700

    Added Precinct struct and Precincts array of Precinct

---
+++ iOS8SwiftMapKit/Precinct.swift
@@ -0,0 +1,17 @@
+//
+//  Precinct.swift
+//  iOS8SwiftMapKit
+//
+//  Created by Kenneth Wilcox on 1/5/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+
+struct Precinct {
+  var precinctNo: String!
+  var address: String!
+  var name: String!
+  var latitude: Double!
+  var longitude: Double!
+}
